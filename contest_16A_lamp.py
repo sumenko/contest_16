57afe457f82a6bc878ae96f451a4c2def1b93bbc
Vladimir Sumenko <3760097@gmail.com>
2021-06-26 17:49:17 +0300

    recoursion solution - done

--- contest_16A_lamp.py
+++ contest_16A_lamp.py
@@ -7,16 +7,29 @@ class Node:
         self.right = right  
         self.left = left
 
-def solution(Node) -> int:
-    max_value = 0
-    while 1:
-        
-        max_value = max(max_value, new_value)
 
+def get_max(Node, max_lamp = None):
+    if not Node:
+        return max_lamp
+
+    if not Node.left and not Node.right:
+        return Node.value
+
+    if not max_lamp: max_lamp = Node.value
+    return max(Node.value, get_max(Node.left, max_lamp), get_max(Node.right, max_lamp))
+
+
+def solution(Node) -> int:
+    return get_max(Node)
+    
 
 if __name__ == '__main__':
-    # my_solution.py
-    left = Node(3)
-    right = Node(5)
-    root = Node(1, left, right)
+    left_1 = Node(14)
+    right_1 = Node(1)
+    left_root = Node(8, left_1, right_1)
     
+    left_2 = Node(10)
+    right_2 = Node(5)
+    right_root = Node(1, left_2, right_2)
+    root2 = Node(100, left_root, right_root) 
+    print(solution(root2))
